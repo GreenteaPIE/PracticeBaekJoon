5612277248c24950454582495eb9b459514bf406
GreenteaPIE <secret48156@gmail.com>
2023-07-03 19:33:53 +0900

    0703

---
+++ BaekJoon/src/BronzeV/AplusB1000.java
@@ -0,0 +1,18 @@
+package BronzeV;
+
+import java.util.Scanner;
+
+public class AplusB1000 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		scanner.close(); 
+		
+		System.out.println(a + b);
+			
+	}
+}
